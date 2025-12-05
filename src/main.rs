7755884b64f0075daee905d7373aa0291c5aef2f
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-05 21:24:39 +0900

    main関数の実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut service_type = String::new();
+    println!("実行したい内容を入力してください(0:登録、1:集計)");
+    io::stdin().read_line(&mut service_type).unwrap();
+    let service_type: u8 = service_type.trim().parse().expect("数値で入力してください");
+    // 入力値のバリデーション
+
+    if service_type == 0 {
+        println!("登録サービス")
+    } else if service_type == 1 {
+        println!("集計サービス")
+    }
 }
